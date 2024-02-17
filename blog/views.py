5e53576cd4776ed3c47664b2a6c79d13f4a476a6
vagabondboffin <z.seyedghorban@gmail.com>
2024-02-17 18:05:27 +0330

    added post model

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from .models import Post
 
 # def home(request):
 # 	return HttpResponse('<h1>Blog Home</h1>')
-posts = [
- 	{
-	 	'author': 'Vaga',
-	 	'title': 'blog post',
-	 	'content': 'first post content',
-	 	'date_posted': 'Aug 25, 2019'
-	},
-	{
-		'author': 'jane',
-	 	'title': 'blog post 2',
-	 	'content': '2nd post content',
-	 	'date_posted': 'Aug 28, 2019'
-	}
- ]
+# posts = [
+#  	{
+# 	 	'author': 'Vaga',
+# 	 	'title': 'blog post',
+# 	 	'content': 'first post content',
+# 	 	'date_posted': 'Aug 25, 2019'
+# 	},
+# 	{
+# 		'author': 'jane',
+# 	 	'title': 'blog post 2',
+# 	 	'content': '2nd post content',
+# 	 	'date_posted': 'Aug 28, 2019'
+# 	}
+#  ]
+
 
 
 def home(request):
 	context = {
-		'posts': posts #accessable from template
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
