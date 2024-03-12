73111b1bae24838c25ec65be70eec1a9726f309c
vagabondboffin <z.seyedghorban@gmail.com>
2024-03-12 15:24:59 +0330

    added delete and update pages

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 #from django.http import HttpResponse
 from .models import Post
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	CreateView,
+	UpdateView,
+	DeleteView
+)
 
 # def home(request):
 # 	return HttpResponse('<h1>Blog Home</h1>')
@@ -26,6 +34,46 @@ def home(request):
 		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html' #unless it would look for blog/post_list.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	
+	def test_func(self):
+	post = self.get_object()
+	if self.request.user == post.author:
+		return True
+	return False
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 # Default primary key field type
@@ -135,3 +138,4 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = 'login'
+
