c824540bfbe4dbbe4a943789651699d31be07d6d
vagabondboffin <z.seyedghorban@gmail.com>
2024-05-05 22:37:38 +0330

    password reset by email

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -139,3 +139,9 @@ AUTHENTICATION_BACKENDS = [
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gamil.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
